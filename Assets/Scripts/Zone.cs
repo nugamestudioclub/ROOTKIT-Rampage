6967f208a007ba268f4fce2a8c6d392dc0bd81f3
sjkobori <sjkobori@gmail.com>
2023-02-05 15:09:57 -0500

    made enemies sleep on start

--- Assets/Scripts/Zone.cs
+++ Assets/Scripts/Zone.cs
@@ -1,16 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Zone : MonoBehaviour
 {
     private readonly HashSet<Enemy> enemies = new HashSet<Enemy>();
 
+    void Start()
+    {
+        StartCoroutine(DoStart());
+    }
+
+    private IEnumerator DoStart()
+    {
+        yield return new WaitForSeconds(1.5f);
+        PutEnemiesToSleep(true);
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         var obj = collision.gameObject;
         if (obj.CompareTag("Player"))
-            foreach (var enemy in enemies)
-                enemy.Asleep = false;
+            PutEnemiesToSleep(false);
         else if (obj.TryGetComponent<Enemy>(out var enemy))
             enemies.Add(enemy);
     }
@@ -19,7 +30,12 @@ public class Zone : MonoBehaviour
     {
         var obj = collision.gameObject;
         if (obj.CompareTag("Player"))
-            foreach (var enemy in enemies)
-                enemy.Asleep = true;
+            PutEnemiesToSleep(true);
+    }
+
+    private void PutEnemiesToSleep(bool value)
+    {
+        foreach (var enemy in enemies)
+            enemy.Asleep = value;
     }
 }
