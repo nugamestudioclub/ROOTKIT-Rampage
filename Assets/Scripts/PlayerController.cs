9111381cf02afc04b4e5face7dc79bb1667b508d
kevin-gator <116971593+kevin-gator@users.noreply.github.com>
2023-02-05 12:14:59 -0500

    More on previous commit
    ...

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -196,6 +196,7 @@ public class PlayerController : MonoBehaviour
     {
         GameObject firedBullet = Instantiate(bullet, barrel.position, Quaternion.identity);
         firedBullet.GetComponent<Rigidbody2D>().velocity = barrel.up * -_fireSpeed;
+        AudioManager.Instance.Balloon();
     }
 
     public void Decoy()
