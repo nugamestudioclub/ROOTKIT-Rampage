ccad8c2f38fef3bea6158eb1b66ae8f2b46a4386
sjkobori <sjkobori@gmail.com>
2023-02-05 15:18:14 -0500

    fixed enemies asleep when starting inside a level

--- Assets/Scripts/Zone.cs
+++ Assets/Scripts/Zone.cs
@@ -6,6 +6,8 @@ public class Zone : MonoBehaviour
 {
     private readonly HashSet<Enemy> enemies = new HashSet<Enemy>();
 
+    private bool hasPlayer;
+
     void Start()
     {
         StartCoroutine(DoStart());
@@ -14,23 +16,31 @@ public class Zone : MonoBehaviour
     private IEnumerator DoStart()
     {
         yield return new WaitForSeconds(1.5f);
-        PutEnemiesToSleep(true);
+        PutEnemiesToSleep(!hasPlayer);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         var obj = collision.gameObject;
         if (obj.CompareTag("Player"))
+        {
+            hasPlayer = true;
             PutEnemiesToSleep(false);
+        }
         else if (obj.TryGetComponent<Enemy>(out var enemy))
+        {
             enemies.Add(enemy);
+        }
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {
         var obj = collision.gameObject;
         if (obj.CompareTag("Player"))
+        {
+            hasPlayer = false;
             PutEnemiesToSleep(true);
+        }
     }
 
     private void PutEnemiesToSleep(bool value)
